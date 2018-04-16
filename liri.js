44adfac2ca36edbf6f6e5e05ca2218a88bc22a89
Matt Brzowski <5thhand@gmail.com>
2018-04-16 13:56:03 -0600

    Initial commit with class-provided code

---
+++ liri.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var arg1 = process.argv[2];
+
+switch (arg1) {
+  case "my-tweets": 
+    console.log("Showing tweets...");
+    break;
+
+  case "spotify-this-song": 
+    if (typeof process.argv[3] === string) {
+      console.log("Spotify-ing your song...");
+    } else {
+      console.log("You must give me a song to Spotify.");
+    }
+    break;
+
+  default:  
+    console.log("Invalid command. See help for instructions on how to use Liri");
+}
